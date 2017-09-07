b9784c7425afabaed4ed0c0370c5103353741b4a
Hameed Goudat <hameedg@agrisoftdg.com>
2017-09-07 12:06:10 -0500

    changed add plant image click event.

--- src/pages/plants/create-plant/create-plant.ts
+++ src/pages/plants/create-plant/create-plant.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { Camera, CameraOptions } from "@ionic-native/camera";
 
 /**
@@ -25,29 +25,41 @@ export class CreatePlantPage {
     timeEnds: '1990-02-20'
   }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera) {
+  constructor(public navCtrl: NavController, public toast: ToastController, public navParams: NavParams, private camera: Camera) {
   }
 
   currentImage: any = 'assets/images/new-image.png';
-  
-    options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE,
-      correctOrientation: true
-    }
-  
-    async takePhoto() {
-      this.camera.getPicture(this.options).then((imageData) => {
-        // imageData is either a base64 encoded string or a file URI
-        // If it's base64:
+
+  options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE,
+    correctOrientation: true
+  }
+
+  async takePhoto() {
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      try {
         let base64Image = 'data:image/jpeg;base64,' + imageData;
-        this.currentImage = base64Image;
-       }, (err) => {
-        this.currentImage = err;
-       });
-    }
+        if (base64Image != null) {
+          this.currentImage = base64Image;
+        } else {
+          this.currentImage = 'assets/images/new-image.png';
+        }
+      } catch (error) {
+
+      }
+
+    }, (err) => {
+      this.toast.create({
+        message: err,
+        duration: 3000
+      })
+    });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreatePlantPage');
